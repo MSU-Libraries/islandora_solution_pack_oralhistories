1b4d9999bd832715d8e549458fc34915998952d0
Sruthin Gaddam <gaddamsr@msu.edu>
2016-05-31 11:06:13 -0400

    Removed parent collection code

--- theme/islandora-oralhistories.tpl.php
+++ theme/islandora-oralhistories.tpl.php
@@ -28,16 +28,7 @@
   </div>
   <div class="islandora-oralhistories-metadata">
     <?php print $description; ?>
-    <?php if ($parent_collections): ?>
-      <div>
-       
-        <ul>
-          <?php foreach ($parent_collections as $collection): ?>
-        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
+    
     <?php print $metadata; ?>
   </div>
 </div>
